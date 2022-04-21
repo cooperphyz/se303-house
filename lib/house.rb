3522c9d29330d643775a40e9b931bc6a2f3f5ec1
cooperphyz <gjcooper99@gmail.com>
2022-04-21 12:04:23 -0700

    Replace line 4 with line(4) call

--- lib/house.rb
+++ lib/house.rb
@@ -32,7 +32,7 @@ class House
         "#{line(1)}\n" +
         "#{line(2)}\n" +
         "#{line(3)}\n" +
-        "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n\n" +
+        "#{line(4)}\n" +
         "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n\n" +
         "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n\n" +
         "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n\n" +
