05be53a5c0318c8d1c801490fa683107fcdaf5fe
cooperphyz <gjcooper99@gmail.com>
2022-04-21 12:08:33 -0700

    Replace line 10 with line(10) call

--- lib/house.rb
+++ lib/house.rb
@@ -38,7 +38,7 @@ class House
         "#{line(7)}\n" +
         "#{line(8)}\n" +
         "#{line(9)}\n" +
-        "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n\n" +
+        "#{line(10)}\n" +
         "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n\n" +
         "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     end
