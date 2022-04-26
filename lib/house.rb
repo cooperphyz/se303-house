265f0873c744db9a1bc3cfbd4c537333d18e697b
cooperphyz <gjcooper99@gmail.com>
2022-04-26 16:55:39 -0700

    Remove line_numbers

--- lib/house.rb
+++ lib/house.rb
@@ -29,7 +29,6 @@ class House
     end
 
     def recite
-        line_numbers = [1,2,3,4,5,6,7,8,9,10,11,12]
         result = (1..12).map { |number| line(number) }.join("\n")
         return result
     end
