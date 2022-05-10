f610dc076827d75f499f55c2c0dc66209819e659
cooperphyz <gjcooper99@gmail.com>
2022-05-10 11:36:27 -0700

    Make when 2 and else the same

--- lib/house.rb
+++ lib/house.rb
@@ -4,7 +4,7 @@ class House
         when 1
             "This is the house that Jack built.\n"
         when 2
-            "This is the malt that lay in the house that Jack built.\n"
+            "This is the #{horse_and_hound(number)}#{farmer_sowing(number)}#{rooster_crowed(number)}#{priest_all_shaven(number)}#{man_all_tattered(number)}#{maiden_all_forelorn(number)}#{cow_with_crumpled(number)}#{dog_that_worried(number)}#{cat_that_killed(number)}#{rat_that_ate(number)}malt that lay in the house that Jack built.\n"
         else
             "This is the #{horse_and_hound(number)}#{farmer_sowing(number)}#{rooster_crowed(number)}#{priest_all_shaven(number)}#{man_all_tattered(number)}#{maiden_all_forelorn(number)}#{cow_with_crumpled(number)}#{dog_that_worried(number)}#{cat_that_killed(number)}#{rat_that_ate(number)}malt that lay in the house that Jack built.\n"
         end
