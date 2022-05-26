53ebbae25113aff6ab82304c247a76df51f8aed1
cooperphyz <gjcooper99@gmail.com>
2022-05-26 11:06:33 -0700

    Add entire song as data field for use in last()

--- lib/house.rb
+++ lib/house.rb
@@ -1,6 +1,23 @@
 class HouseNumber
     attr_reader :number
 
+    def fragments
+        [
+          'horse and the hound and the horn that belonged to the ',
+          'farmer sowing his corn that kept the ',
+          'rooster that crowed in the morn that woke the ',
+          'priest all shaven and shorn that married the ',
+          'man all tattered and torn that kissed the ',
+          'maiden all forlorn that milked the ',
+          'cow with the crumpled horn that tossed the ',
+          'dog that worried the ',
+          'cat that killed the ',
+          'rat that ate the ',
+          'malt that lay in the ',
+          'house that Jack built',
+        ]
+    end
+
     def initialize(number)
         @number = number
     end
@@ -96,7 +113,7 @@ end
 
 class House
     def line(number)
-        house_number = HouseNumberPirate.new(number)
+        house_number = HouseNumber.new(number)
 
         "#{house_number}house that Jack built.\n"
     end
