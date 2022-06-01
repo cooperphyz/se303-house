cd64d0dd01f7169c314a24213f3a3ab363c74184
cooperphyz <gjcooper99@gmail.com>
2022-05-31 18:35:07 -0700

    Remove unused fragments in House

--- lib/house.rb
+++ lib/house.rb
@@ -12,22 +12,6 @@ class House
         "#{opener}the %s.\n" % verses.last(number).join('')
     end
 
-    def fragments
-        [
-          'horse and the hound and the horn that belonged to the ',
-          'farmer sowing his corn that kept the ',
-          'rooster that crowed in the morn that woke the ',
-          'priest all shaven and shorn that married the ',
-          'man all tattered and torn that kissed the ',
-          'maiden all forlorn that milked the ',
-          'cow with the crumpled horn that tossed the ',
-          'dog that worried the ',
-          'cat that killed the ',
-          'rat that ate the ',
-          'malt that lay in the '
-        ]
-    end
-
     def recite
         (1..12).map { |number| line(number) }.join("\n")
     end
