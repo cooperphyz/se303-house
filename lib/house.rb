e0dd0212a5da120b7b20b9df788b9c98ee5b89b9
cooperphyz <gjcooper99@gmail.com>
2022-05-31 19:11:01 -0700

    Split fragments into subject/verse clauses

--- lib/house.rb
+++ lib/house.rb
@@ -25,27 +25,29 @@ class HouseVerse
 
     def lyrics
         newArrayOne, newArrayTwo = fragments.partition.with_index { |_,i| i.even? }
-        newArrayOne.shuffle!
-        newArrayTwo.shuffle!
+        #newArrayOne.shuffle!
+        #newArrayTwo.shuffle!
 
-        newArrayThree = Hash[newArrayOne.zip(newArrayTwo)].map { |k, v| "#{k}#{v}" }
+        newArrayThree = Hash[newArrayOne.zip(newArrayTwo)].map { |k, v| "#{k}#{v}" }.append('house that Jack built')
 
-        return (randomized ? fragments.shuffle : fragments).append('house that Jack built')
+        return newArrayThree
+        
+        #return (randomized ? fragments.shuffle : fragments).append('house that Jack built')    
     end
     
     def fragments
         [
-          'horse and the hound and the horn that belonged to the ',
-          'farmer sowing his corn that kept the ',
-          'rooster that crowed in the morn that woke the ',
-          'priest all shaven and shorn that married the ',
-          'man all tattered and torn that kissed the ',
-          'maiden all forlorn that milked the ',
-          'cow with the crumpled horn that tossed the ',
-          'dog that worried the ',
-          'cat that killed the ',
-          'rat that ate the ',
-          'malt that lay in the '
+          'horse and the hound and the horn that ', 'belonged to the ',
+          'farmer sowing his corn that ', 'kept the ',
+          'rooster that crowed in the morn that ', 'woke the ',
+          'priest all shaven and shorn that ', 'married the ',
+          'man all tattered and torn that ', 'kissed the ',
+          'maiden all forlorn that ', 'milked the ',
+          'cow with the crumpled horn that ', 'tossed the ',
+          'dog that ', 'worried the ',
+          'cat that ', 'killed the ',
+          'rat that ', 'ate the ',
+          'malt that ', 'lay in the '
         ]
     end
 end
